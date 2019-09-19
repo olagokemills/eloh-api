bf489a50df7a9f45dcaca2d94c634a750d2a6994
olagokemills <olagokemills@gmail.com>
2019-09-19 23:34:46 +0100

    added  db

---
+++ config/config.js
@@ -0,0 +1,4 @@
+module.exports = {
+    PORT: 3000,
+    dbUrl: 'mongodb://127.0.0.1:27017/eloh'
+}
--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 const express = require('express');
-const bodyParser = require('body-parser')
+const bodyParser = require('body-parser');
+const config = require('./config/config');
+const cors = require('cors');
+const mongoose = require('mongoose');
 
 const app = express();
 
-
+//cors request
+app.use(cors());
 //Parse requests
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
@@ -15,5 +19,18 @@ app.get('/', (req, res) => {
     res.json({"message": "Welcome, human"})
 })
 
+//db connect
+mongoose.connect(config.dbUrl, {
+    useNewUrlParser: true
+}).then(()=> {
+    console.log('Connected to DB');
+}).catch(err => {
+    console.log('Connction failed');
+    process.exit();
+})
 
-//post to
+
+//port to listen to
+app.listen(config.PORT, () => {
+    console.log("Server is live on your desired port");
+})
